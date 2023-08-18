46e1601c57e3c3f57dbf68278f7356e4de44edda
nihilisticneuralnet <138315505+nihilisticneuralnet@users.noreply.github.com>
2023-08-18 16:49:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def image_loader(image_name,target_shape=(imsize, imsize)):
     image = loader(image).unsqueeze(0)
     return image.to(device,torch.float)
 
-style_img = image_loader('starrynight.png')
-content_img = image_loader('mozart.png')
+style_img = image_loader('style.png')
+content_img = image_loader('content.png')
 
 print("Style image shape:", style_img.shape)
 print("Content image shape:", content_img.shape)
@@ -157,8 +157,88 @@ def get_style_model_and_losses(
         
     model = model[:(i+1)]
     return model,style_losses,content_losses    
-    
+
 input_img = content_img.clone()
 
 plt.figure()
-imshow(input_img,title='Output Image')
+imshow(input_img,title='Input Image')
+
+def get_input_optimizer(input_img):
+    optimizer = optim.LBFGS([input_img])
+    return optimizer
+def run_style_transfer(
+    cnn,
+    normalization_mean,
+    normalization_std,
+    content_img,
+    style_img,
+    input_img,
+    num_steps=500,
+    style_weight=100000,
+    content_weight=1
+                      ):
+    
+    print('Building the style transfer model..')
+    model, style_losses, content_losses = get_style_model_and_losses(cnn,normalization_mean,normalization_std,style_img,content_img)
+    
+    input_img.requires_grad_(True)
+    model.requires_grad_(False)
+    
+    optimizer = get_input_optimizer(input_img)
+    
+    print('Optimizing...')
+    run = [0]
+    
+    while run[0] <= num_steps:
+        
+        def closure():
+            
+            with torch.no_grad():
+                input_img.clamp_(0,1)
+                
+            optimizer.zero_grad()
+            model(input_img)
+            style_score=0
+            content_score=0
+            
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+            
+            style_score *= style_weight
+            content_score *= content_weight
+            
+            loss = style_score + content_score
+            loss.backward()
+            
+            run[0] += 1
+            if run[0]%50 == 0 :
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
+                
+            return style_score + content_score
+        
+        optimizer.step(closure)
+        
+    
+    with torch.no_grad():
+        input_img.clamp_(0,1)
+        
+    
+    return input_img
+output = run_style_transfer(cnn,cnn_normalization_mean,cnn_normalization_std,content_img,style_img,input_img)
+
+plt.figure()
+imshow(output,title='Generated Image')
+
+plt.ioff()
+plt.show()
+
+# Save the PIL image to a file
+output_image = output.squeeze(0).cpu().clone()
+output_image = unloader(output_image)
+output_image = output_image.resize((720, 720), Image.ANTIALIAS)
+output_image.save('generated.png') 
